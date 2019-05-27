0f174bf8151d325327792e66ffc4004241068b0c
Ramiz666 <49520893+Ramiz666@users.noreply.github.com>
2019-05-27 13:59:56 +0400

    Add files via upload

---
+++ 20 may/js/main.js
@@ -0,0 +1,24 @@
+
+
+
+// let array = [2, 4, 6, 8, 10];
+// const mapp = array.map(x => x * 2);
+// console.log(mapp)
+
+
+
+// let numbers = [45, 4, 9, 16, 25];
+// let bolunen = numbers.filter(myFunction);
+
+// function myFunction(value, index, array) {
+//   return value % 5 === 0;
+// }
+// console.log (bolunen)
+
+
+// let numbers1 = [45, 4, 9, 16, 25];
+// let sum = numbers1.reduce(myFunction);
+
+// function myFunction(total, value, index, array) {
+//   return total + value;
+// }
